a5c23e8e7cd56cbe7cb58333e91767dd2ad9d083
jainabhishek2004 <157394189+jainabhishek2004@users.noreply.github.com>
2024-11-13 01:39:35 +0530

    Update Hero.tsx

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 
-
 export default function Hero() {
   return (
-    <section className="flex flex-col md:flex-row bg-[#FEEEDD] p-8 md:items-center justify-center space-y-6 md:space-y-0">
+    <section className="flex flex-col md:flex-row bg-[#FEEEDD] p-10 md:p-12 lg:p-16 md:items-center justify-center space-y-6 md:space-y-0">
       {/* Text Section */}
       <div className="md:w-1/2 space-y-4">
         <h1 className="text-5xl font-bold" style={{ fontFamily: 'Spicy Rice, cursive', color: '#013D4D' }}>
@@ -13,31 +12,36 @@ export default function Hero() {
           Indian Society for Technical Education
         </h2>
         <p className="text-gray-700" style={{ fontFamily: 'Work Sans, sans-serif', color: '#013D4D' }}>
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-          consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Lorem ipsum dolor sit amet,
-           consectetur adipisicing elit. Iste esse a aperiam quos blanditiis? In ab eligendi asperiores
-           <br />
-           <br />
-          praesentium recusandae ad nulla consequuntur officia sunt minus numquam reiciendis, tenetur unde.
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium cupiditate reprehenderit omnis nam, soluta,
-          et assumenda sunt molestiae quidem mollitia velit, cumque iste deserunt quasi.vitae hic qui molestias quis
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, saepe sit natus excepturi quisquam impedit? Ducimus debitis
+          iure tenetur sunt quod corporis blanditiis, illum quidem 
+          doloribus, aliquam sed fugit enim?
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, saepe sit natus excepturi quisquam impedit? Ducimus debitis
+          iure tenetur sunt quod corporis blanditiis, illum quidem 
+          doloribus, aliquam sed fugit enim?
         </p>
       </div>
 
-
-      {/* Image Section with Double Frame */}
+      {/* Image Section with Double Frame Effect */}
       <div className="md:w-1/2 flex justify-center md:justify-end">
-        <div className="relative border-4 border-[#2B2B2B] p-3">
-          <div className="absolute inset-0 translate-x-3 translate-y-3 bg-[#FEEEDD] border-4 border-[#2B2B2B]"></div>
-          <Image
-            src="/assets/hero_image_3_v1.png"  // Replace with your actual image path
-            alt="Group Photo"
-            width={500}
-            height={300}
-            className="relative z-10 rounded-lg border-2 border-black-400 shadow-lg"
-          />
+        <div className="relative p-4">
+          {/* Outer Frame with rounded bottom-right corner */}
+          <div className="absolute right-0 bottom-0 w-full h-full border-r-8 border-b-8 border-[#013D4D] translate-x-2 translate-y-2 rounded-br-2xl"></div>
+
+          {/* Inner Frame */}
+          <div className="relative border-8 border-[#013D4D] p-2 bg-[#FEEEDD] rounded-2xl">
+            <Image
+              src="/assets/story_image.png"  
+              alt="Story Image"
+              width={600}  
+              height={350}  
+              className="object-cover rounded-lg"
+            />
+          </div>
         </div>
       </div>
     </section>
   );
 }
+
